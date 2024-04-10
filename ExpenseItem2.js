d90b6057601839bae82d5d46eee173e2ec40200b
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-04-10 16:04:47 +0530

    assign1 task4

---
+++ ExpenseItem2.js
@@ -0,0 +1,15 @@
+// Write your code here:
+function ExpenseItem(){
+    return (
+      <div>
+          <div>15 August 2023</div>
+          <div>Book</div>
+          <div>$10</div>
+          <div>Delhi</div> 
+        </div>
+      
+    );
+      
+    }
+    
+    export default ExpenseItem;

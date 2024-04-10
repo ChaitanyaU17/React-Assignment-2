b9dee4e6fb9cd306ae0cc60113f4623f0a9ed8eb
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-04-10 14:36:49 +0530

    assign1 task 3

---
+++ ExpenceItem.js
@@ -0,0 +1,13 @@
+// Write your code here:
+function ExpenceItem() {
+    return (
+      <div>
+         <h2>This is expense item!</h2>
+        <p>Food - $5</p>
+        <p>Petrol - $20</p>
+        <p>Movie - $10</p>
+      </div>
+    );
+  }
+  
+  export default ExpenceItem;
